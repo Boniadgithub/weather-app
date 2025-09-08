7f49ed3f3fdc392f7d2b11dafa458cd51f71664d
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-09-08 02:47:00 -0700

    update themepro

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 
 import './App.css'
 import { BrowserRouter } from 'react-router-dom';
-
+import Layout from './components/Layout';  
+import { ThemeProvider } from './context/theme-provider';
 
 function App() {
 return(
 <BrowserRouter>
-   
+  <ThemeProvider defaultTheme="dark">
+    <Layout> Hello</Layout>
+  </ThemeProvider>
 </BrowserRouter>
 );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,15 @@
-import React, { type PropsWithChildren } from 'react'
+import  type {PropsWithChildren } from 'react'
 
 const Layout = ({children}:PropsWithChildren) => {
   return (
-    <div>
-      {children}
+    <div className='bg-gradient-to-br from-background to-muted'>
+        header
+      <main className='min-h-screen container mx-auto px-4 py-8'>{children}</main>
+        <footer className='border-t backdrop:blur py-4 supports-[backdrop-filter]:bg-background/60'>
+            <div className='container mx-auto text-center text-gray-400'>
+                <p>I am boni</p>
+            </div>
+        </footer>
     </div>
   );  
 };
