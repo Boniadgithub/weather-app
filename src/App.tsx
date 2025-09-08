0ed9587d8c08c74a6b7015b3b938b66d2b3f14d9
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-09-08 01:53:09 -0700

    my first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+import { Button } from '@/components/ui/button'
+
+function App() {
+return<div>my name is Boni
+
+    <Button variant="destructive">clickme</Button>
+</div>
+}
+
+export default App
